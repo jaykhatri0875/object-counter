7eabbe57428b3452c2f08ca31e0ba8fb67d4b6c9
Jay <khatri.jaykumar.0875@ldce.ac.in>
2025-05-14 00:42:38 +0530

    Updating service endpoint to only accept the images

--- counter/entrypoints/webapp.py
+++ counter/entrypoints/webapp.py
@@ -4,6 +4,10 @@ from flask import Flask, request, jsonify
 
 from counter import config
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 def create_app():
     
     app = Flask(__name__)
@@ -12,14 +16,21 @@ def create_app():
     
     @app.route('/object-count', methods=['POST'])
     def object_detection():
-        
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'}), 400
+
         threshold = float(request.form.get('threshold', 0.5))
         uploaded_file = request.files['file']
-        model_name = request.form.get('model_name', "rfcn")
-        image = BytesIO()
-        uploaded_file.save(image)
-        count_response = count_action.execute(image, threshold)
-        return jsonify(count_response)
+
+        if uploaded_file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+
+        if uploaded_file and allowed_file(uploaded_file.filename):
+            model_name = request.form.get('model_name', "rfcn")
+            image = BytesIO()
+            uploaded_file.save(image)
+            count_response = count_action.execute(image, threshold) # TODO : add model name as argument to be passed in ml service
+            return jsonify(count_response)
     
     return app
 
